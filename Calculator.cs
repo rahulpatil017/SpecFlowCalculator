e7e1f6ecaa5ae81b315541bbb3a2f1392ca2f061
Rahul Patil <rahulpatil017@gmail.com>
2021-11-29 12:06:31 -0500

    Initial commit

---
+++ Calculator.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, SpecFlow!");
+/// <summary>
+/// Add two numbers
+/// </summary>
+public class Calculator
+{
+    public int FirstNumber { get; set; }
+    public int SecondNumber { get; set; }
+
+    public int Add()
+    {
+        return FirstNumber + SecondNumber;
+    }
+}
